6d780ebe949e378952aa7027cf574ea907b1faa2
Gowtham Sai <dev@gowtham-sai.com>
2019-03-28 06:07:26 +0530

    Views renders anything with json tags to stdout

--- internal/views/views.go
+++ internal/views/views.go
@@ -1,1 +1,16 @@
 package views
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gojektech/weaver/pkg/logger"
+)
+
+func Render(o interface{}) {
+	jsonBytes, err := json.MarshalIndent(o, "", "    ")
+	if err != nil {
+		logger.Fatalf("Error marshaling outptu: %s", err)
+		panic(err)
+	}
+	fmt.Println(string(jsonBytes))
+}
